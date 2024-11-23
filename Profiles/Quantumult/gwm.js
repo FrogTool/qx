439380c6c5beb6660f6c0b4ffd2a345fee3519fa
zhou <azoon@qq.com>
2024-11-23 17:16:58 +0800

    Update gwm.js

--- Profiles/Quantumult/gwm.js
+++ Profiles/Quantumult/gwm.js
@@ -7,9 +7,5 @@ if (body) {
     $notify(`🎉${cookieName} 获取成功！`, '', `${notifyName}=${body}`);
 }
 
-$notify(`${cookieName}获取成功！`, '', `${cookieName}获取成功！请查看弹窗匹配值或日志查看完整值。`);
-console.log(`${cookieName}获取成功！`);
-console.log(`🔔输出完整请求值：\n${body}\n`);
-
-setTimeout($done, 1000)
+setTimeout($done, 100000)
 $done({})
